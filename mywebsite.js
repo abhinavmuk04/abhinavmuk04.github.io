29d132f749768351f58923fc54c4ad9c6b12c10a
abhinavmuk04 <69162586+abhinavmuk04@users.noreply.github.com>
2023-05-02 17:32:04 -0400

    Add files via upload

---
+++ mywebsite.js
@@ -0,0 +1,21 @@
+data = [{
+    "name": "Tony Stark",
+    "strength": 6.5,
+    "intelligence": 8
+},
+{
+    "name": "Hulk",
+    "strength": 9,
+    "intelligence": 3
+},
+{
+    "name": "Doctor Strange",
+    "strength": 5,
+    "intelligence": 9.5
+},
+{
+    "name": "Spider Man",
+    "strength": 6.5,
+    "intelligence": 7
+}]
+console.log(data)
